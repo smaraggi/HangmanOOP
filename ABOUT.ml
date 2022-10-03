c5fea8fac7eddfe1203736873aa6bd58b8668651
Santiago Maraggi <smaraggi@gmail.com>
2022-10-03 19:06:36 -0300

    Adding about file

---
+++ ABOUT.ml
@@ -0,0 +1,9 @@
+# Algoritmos y Programación III
+## Universidad de Buenos Aires - Facultad de Ingeniería
+### 2do cuatrimestre de 2023
+
+#### Ejemplo de Programación Orientada a Objetos
+#### Juego El ahorcado
+
+#### Descripción: se implementa el patrón MVC. No se implementan otros patrones OOP que se verán más adelante en la materia. Se aplican principios de diseño OOP.
+
